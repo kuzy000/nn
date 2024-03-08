beb17b84a8fc8ceebac163d28753fbc52459aa42
Egor Moiseenko <kuzy000@gmail.com>
2024-03-09 02:53:34 +0300

    Colored furb

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::time::Duration;
 use burn::backend::NdArray;
 use burn::module::Module;
 use burn::nn::{Linear, LinearConfig};
-use burn::optim::{GradientsParams, Optimizer, SgdConfig};
+use burn::optim::{AdamConfig, GradientsParams, Optimizer, SgdConfig};
 use burn::tensor::activation::{relu, sigmoid};
 use burn::tensor::backend::{AutodiffBackend, Backend};
 
@@ -25,7 +25,10 @@ struct Model<B: Backend> {
     ln: Vec<Linear<B>>,
 }
 
-pub fn leaky_relu<const D: usize, B: Backend, E: ElementConversion>(tensor: Tensor<B, D>, slope: E) -> Tensor<B, D> {
+pub fn leaky_relu<const D: usize, B: Backend, E: ElementConversion>(
+    tensor: Tensor<B, D>,
+    slope: E,
+) -> Tensor<B, D> {
     let leak = tensor.clone().clamp_min(0.).mul_scalar(slope);
     tensor.clamp_min(0.) + leak
 }
@@ -108,7 +111,7 @@ fn try_main() -> Result<()> {
     main_backend::<burn::backend::Autodiff<burn::backend::Wgpu>>(device)
 }
 
-fn img(s: &str) -> Result<(Vec<f32>, u32, u32)> {
+fn img_grayscale(s: &str) -> Result<(Vec<f32>, u32, u32)> {
     let image = image::io::Reader::open(s)?.decode()?;
     let image = image.grayscale().to_luma32f();
     let w = image.width();
@@ -116,6 +119,30 @@ fn img(s: &str) -> Result<(Vec<f32>, u32, u32)> {
     let image = image.to_vec().iter().map(|x| 1. - x).collect();
 
     Ok((image, w, h))
+}
+
+fn img_yuv(s: &str) -> Result<(Vec<f32>, u32, u32)> {
+    let image = image::io::Reader::open(s)?.decode()?;
+    let image = image.to_rgb32f();
+    let w = image.width();
+    let h = image.height();
+    let mut image = image.to_vec();
+    transform_color(&mut image, rgb_to_yuv);
+
+    Ok((image, w, h))
+}
+
+fn transform_color<F>(image: &mut Vec<f32>, f: F)
+where
+    F: Fn(f32, f32, f32) -> [f32; 3],
+{
+    for i in (0..image.len()).step_by(3) {
+        let (r, g, b) = (image[i + 0], image[i + 1], image[i + 2]);
+        let [y, u, v] = f(r, g, b);
+        image[i + 0] = y;
+        image[i + 1] = u;
+        image[i + 2] = v;
+    }
 }
 
 fn map_range(a0: f32, b0: f32, a1: f32, b1: f32, v: f32) -> f32 {
@@ -136,6 +163,22 @@ fn make_coords(w: u32, h: u32, third: f32, offset: f32) -> Vec<f32> {
         .collect()
 }
 
+fn rgb_to_yuv(r: f32, g: f32, b: f32) -> [f32; 3] {
+    [
+        r * 0.2126 + g * 0.7152 + b * 0.0722,
+        r * -0.09991 + g * -0.33609 + b * 0.436,
+        r * 0.615 + g * -0.55861 + b * -0.05639,
+    ]
+}
+
+fn yuv_to_rgb(y: f32, u: f32, v: f32) -> [f32; 3] {
+    [
+        y * 1.0 + u * 0.0 + v * 1.28033,
+        y * 1.0 + u * -0.21482 + v * -0.38059,
+        y * 1.0 + u * 2.12798 + v * 0.0,
+    ]
+}
+
 fn main_backend<B: AutodiffBackend<FloatElem = f32>>(device: B::Device) -> Result<()> {
     let ui_state = Arc::new(Mutex::new(UiState::new()));
     {
@@ -143,8 +186,8 @@ fn main_backend<B: AutodiffBackend<FloatElem = f32>>(device: B::Device) -> Resul
         thread::spawn(move || ui_main(s));
     }
 
-    let (image0, w, h) = img("f0.png")?;
-    let (image1, _, _) = img("f2.png")?;
+    let (image0, w, h) = img_yuv("furby.png")?;
+    let (image1, _, _) = img_yuv("furby2.png")?;
 
     let coords0 = make_coords(w, h, 0., 0.);
     let coords1 = make_coords(w, h, 1., 0.);
@@ -186,9 +229,9 @@ fn main_backend<B: AutodiffBackend<FloatElem = f32>>(device: B::Device) -> Resul
     );
     // let ti = Tensor::from_vec(coords, ((w * h) as usize, 2), &device)?.to_dtype(candle_core::DType::F16)?;
 
-    assert_eq!(image.len(), (w * h * 2) as usize);
+    assert_eq!(image.len(), (w * h * 2 * 3) as usize);
     let to = Tensor::<B, 2>::from_floats(
-        Data::new(image, Shape::new([(w * h * 2) as usize, 1])),
+        Data::new(image, Shape::new([(w * h * 2) as usize, 3])),
         &device,
     );
     // let to = Tensor::from_slice(&image, ((w * h) as usize, 1), &device)?.to_dtype(candle_core::DType::F16)?;
@@ -197,45 +240,48 @@ fn main_backend<B: AutodiffBackend<FloatElem = f32>>(device: B::Device) -> Resul
     //let tt = Tensor::from_vec(coords_th, ((tw * th) as usize, 2), &device)?.to_dtype(candle_core::DType::F16)?;
 
     // let mut model = Model::new(&[2, 20, 20, 20, 1], &device)?;
-    let mut model = Model::new(&[3, 512, 512, 512, 1], &device)?;
+    let mut model = Model::new(&[3, 200, 200, 200, 200, 200, 3], &device)?;
 
-    ui_state.lock().unwrap().rate = 0.01;
+    ui_state.lock().unwrap().rate = 0.001;
     //let mut sgd = SGD::new(varmap.all_vars(), ui_state.lock().unwrap().rate as f64)?;
 
-    let sdg_config = SgdConfig::new();
-    let mut sgd = sdg_config.init();
+    //let sdg_config = SgdConfig::new();
+    //let mut opt = sdg_config.init();
+    
+    let adam_config = AdamConfig::new();
+    let mut opt = adam_config.init();
 
     for i in 1.. {
-        let f = model.forward(ti.clone());
-        let loss = f.clone().sub(to.clone()).powf_scalar(2.).sum().sqrt();
+        if !ui_state.lock().unwrap().pause {
+            let f = model.forward(ti.clone());
+            let loss = f.clone().sub(to.clone()).powf_scalar(2.).sum().sqrt();
 
-        let grads = loss.backward();
-        let grads = GradientsParams::from_grads(grads, &model);
+            let grads = loss.backward();
+            let grads = GradientsParams::from_grads(grads, &model);
 
-        model = sgd.step(ui_state.lock().unwrap().rate as f64, model, grads);
+            model = opt.step(ui_state.lock().unwrap().rate as f64, model, grads);
 
-        if i % 2 != 0 {
-            continue;
+
+            // if i % 1000 == 0 {
+            //     let tf = model.forward(&tt)?;
+            //     let res: Vec<f32> = tf.squeeze(1)?.to_dtype(candle_core::DType::F32)?.to_vec1()?;
+            //     let res = res.iter().map(|x| (x * 255.) as u8).collect::<Vec<u8>>();
+
+            //     let image: GrayImage = ImageBuffer::from_raw(tw, th, res).unwrap();
+            //     image.save("out.png")?;
+            // }
+
+            let loss = loss.into_scalar();
+
+            // sgd.set_learning_rate((ui_state.lock().unwrap().rate / loss) as f64);
+
+            // assert!(!loss.is_nan());
+
+            ui_state.lock().unwrap().loss.push(loss);
+            ui_state.lock().as_mut().unwrap().epoch = i;
         }
 
-        // if i % 1000 == 0 {
-        //     let tf = model.forward(&tt)?;
-        //     let res: Vec<f32> = tf.squeeze(1)?.to_dtype(candle_core::DType::F32)?.to_vec1()?;
-        //     let res = res.iter().map(|x| (x * 255.) as u8).collect::<Vec<u8>>();
-
-        //     let image: GrayImage = ImageBuffer::from_raw(tw, th, res).unwrap();
-        //     image.save("out.png")?;
-        // }
-
-        let loss = loss.into_scalar();
-
-        // sgd.set_learning_rate((ui_state.lock().unwrap().rate / loss) as f64);
-
-        // assert!(!loss.is_nan());
-
-        ui_state.lock().unwrap().loss.push(loss);
-
-        {
+        if i % 10 != 0 || ui_state.lock().unwrap().pause {
             let w = w * 4;
             let h = h * 4;
             ui_state.lock().as_mut().unwrap().size = (w, h);
@@ -248,14 +294,13 @@ fn main_backend<B: AutodiffBackend<FloatElem = f32>>(device: B::Device) -> Resul
             );
             let f = model.forward(ti.clone());
 
-            let res = f.to_data().value;
+            let mut res = f.to_data().value;
+            transform_color(&mut res, yuv_to_rgb);
             ui_state.lock().as_mut().unwrap().img = res;
         }
 
-        ui_state.lock().as_mut().unwrap().epoch = i;
-
-        while ui_state.lock().unwrap().pause {
-            std::thread::sleep(Duration::from_secs_f32(1. / 60.));
+        if ui_state.lock().unwrap().pause {
+            std::thread::sleep(Duration::from_millis(2));
         }
         //std::thread::sleep(Duration::from_secs_f32(0.01));
     }
--- src/ui.rs
+++ src/ui.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use macroquad::{
     prelude::*,
     ui::{hash, root_ui},
@@ -17,7 +18,7 @@ fn draw(state: &mut UiState) {
 
     ui.label(None, &format!("FPS: {}", get_fps()));
     ui.checkbox(hash!(), "Pause", &mut state.pause);
-    ui.slider(hash!(), "Rate", 0.000001..3.0, &mut state.rate);
+    ui.slider(hash!(), "Rate", 0.000001..0.1, &mut state.rate);
     ui.slider(hash!(), "Img", 0.0..1.0, &mut state.slider);
     ui.label(None, &format!("Epoch: {}", state.epoch));
     ui.label(None, &format!("Loss: {}", loss));
@@ -28,10 +29,14 @@ fn draw(state: &mut UiState) {
         let p = canvas.request_space(s);
 
         canvas.rect(Rect::new(p.x, p.y, s.x, s.y), BLACK, None);
+        
+        let size = 250;
 
         let max = *state
             .loss
             .iter()
+            .rev()
+            .take(size)
             .max_by(|x, y| x.partial_cmp(y).unwrap())
             .unwrap_or(&0.);
 
@@ -39,16 +44,15 @@ fn draw(state: &mut UiState) {
             break 'out;
         }
 
-        let num = state.loss.len().min(200);
+        let num = state.loss.len().min(size);
+        let beg = state.loss.len().saturating_sub(size);
 
         for i in 0..num - 1 {
             let xa = (i + 0) as f32 / (num - 1) as f32;
             let xb = (i + 1) as f32 / (num - 1) as f32;
             
-            let factor = state.loss.len() as f32 / num as f32;
-            
-            let ia = ((i + 0) as f32 * factor) as usize;
-            let ib = ((i + 1) as f32 * factor) as usize;
+            let ia = beg + i + 0;
+            let ib = beg + i + 1;
 
             let ya = state.loss.get(ia).unwrap();
             let yb = state.loss.get(ib).unwrap();
@@ -59,6 +63,27 @@ fn draw(state: &mut UiState) {
                 BLACK,
             );
         }
+
+//        let num = state.loss.len().min(200);
+//
+//        for i in 0..num - 1 {
+//            let xa = (i + 0) as f32 / (num - 1) as f32;
+//            let xb = (i + 1) as f32 / (num - 1) as f32;
+//            
+//            let factor = state.loss.len() as f32 / num as f32;
+//            
+//            let ia = ((i + 0) as f32 * factor) as usize;
+//            let ib = ((i + 1) as f32 * factor) as usize;
+//
+//            let ya = state.loss.get(ia).unwrap();
+//            let yb = state.loss.get(ib).unwrap();
+//
+//            canvas.line(
+//                p + vec2(xa, 1. - ya / max) * s,
+//                p + vec2(xb, 1. - yb / max) * s,
+//                BLACK,
+//            );
+//        }
     };
     
     if !state.img.is_empty() {
@@ -66,6 +91,24 @@ fn draw(state: &mut UiState) {
             let v = ((1. - v) * 255.).clamp(0., 255.) as u8;
             [v, v, v, 255]
         }).flatten().collect();
+        
+        let mut buf = Vec::<u8>::new();
+        buf.resize(state.img.len() / 3 * 4, 0);
+
+        { 
+            let mut j = 0;
+            for i in (0..state.img.len()).step_by(3) {
+                let s = &state.img;
+                let (r, g, b) = (s[i + 0], s[i + 1], s[i + 2]);
+                
+                buf[j + 0] = (r * 255.).clamp(0., 255.) as u8;
+                buf[j + 1] = (g * 255.).clamp(0., 255.) as u8;
+                buf[j + 2] = (b * 255.).clamp(0., 255.) as u8;
+                buf[j + 3] = 255;
+
+                j += 4;
+            }
+        }
 
         let texture = Texture2D::from_rgba8(state.size.0 as u16, state.size.1 as u16, &buf);
         texture.set_filter(FilterMode::Nearest);
